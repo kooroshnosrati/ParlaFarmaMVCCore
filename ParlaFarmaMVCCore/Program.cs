803200bed421e23281acce0f831bc09230dddbe2
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-01-13 17:56:32 +0330

    9810231756

--- ParlaFarmaMVCCore/Program.cs
+++ ParlaFarmaMVCCore/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace ParlaFarmaMVCCore
 {
-    public class Program
+    public class Program 
     {
         public static void Main(string[] args)
         {
