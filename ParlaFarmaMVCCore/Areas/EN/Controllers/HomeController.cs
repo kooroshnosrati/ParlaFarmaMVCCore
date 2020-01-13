07921b032e4ad271ed113756a790d77754d62175
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-01-13 18:55:44 +0330

    9810231855

---
+++ ParlaFarmaMVCCore/Areas/EN/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParlaFarmaMVCCore.Areas.EN.Controllers
+{
+    [Area("EN")]
+    [Route("EN/[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ParlaFarmaMVCCore/Startup.cs
+++ ParlaFarmaMVCCore/Startup.cs
@@ -73,8 +73,12 @@ namespace ParlaFarmaMVCCore
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapAreaControllerRoute(
+                     name: "areas", "areas",
+                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
         }
     }
 }
