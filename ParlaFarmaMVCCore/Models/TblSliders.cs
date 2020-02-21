aca6edfa9122845289f440bd644c7c1c3e2b2a4b
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-02-22 00:30:21 +0330

    9812030030

---
+++ ParlaFarmaMVCCore/Models/TblSliders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParlaFarmaMVCCore.Models
+{
+    public partial class TblSliders
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Title3 { get; set; }
+        public string ButtonText { get; set; }
+        public string ButtonLink { get; set; }
+    }
+}
