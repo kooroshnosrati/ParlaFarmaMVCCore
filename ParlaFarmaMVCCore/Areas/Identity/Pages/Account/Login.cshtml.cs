2a9d876978b869b4bf06f4c057d7aabf485fa290
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-02-27 00:24:26 +0330

    9812080025

--- ParlaFarmaMVCCore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ParlaFarmaMVCCore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace ParlaFarmaMVCCore.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/CPanel");
 
             if (ModelState.IsValid)
             {
