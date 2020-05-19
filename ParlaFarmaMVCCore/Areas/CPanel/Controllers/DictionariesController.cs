5c07785b55145f401ca86d2d9d352152f29fccf3
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-05-19 22:02:52 +0430

    9902302202

--- ParlaFarmaMVCCore/Areas/CPanel/Controllers/DictionariesController.cs
+++ ParlaFarmaMVCCore/Areas/CPanel/Controllers/DictionariesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ParlaFarmaMVCCore.Models;
 
 namespace ParlaFarmaMVCCore.Areas.CPanel.Controllers
 {
+    [Authorize]
     [Area("CPanel")]
     public class DictionariesController : Controller
     {
