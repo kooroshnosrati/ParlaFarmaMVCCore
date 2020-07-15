956c6d75b3cf4cf4575f0ba56d51acf408d28df0
koorosh nosrati heravi <koorosh.nosrati@live.com>
2020-07-15 12:36:44 +0430

    9904251236

--- ParlaFarmaMVCCore/Areas/CPanel/Controllers/HomeController.cs
+++ ParlaFarmaMVCCore/Areas/CPanel/Controllers/HomeController.cs
@@ -17,6 +17,5 @@ namespace ParlaFarmaMVCCore.Areas.CPanel.Controllers
         {
             return View();
         }
-        
     }
 }
--- ParlaFarmaMVCCore/Startup.cs
+++ ParlaFarmaMVCCore/Startup.cs
@@ -38,9 +38,11 @@ namespace ParlaFarmaMVCCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ParlaDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).
+                AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
